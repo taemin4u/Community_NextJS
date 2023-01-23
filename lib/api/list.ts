3c34058a3fa7fd15ac2df768c114c1abe193fe89
taemin4u <taemin4u@khu.ac.kr>
2023-01-23 12:53:46 +0900

    230123

--- lib/api/list.ts
+++ lib/api/list.ts
@@ -1,5 +1,6 @@
 import axios from '.';
 import { IList } from '../../types/list';
 
+// GET: /api/[category]
 export const getFeedsAPI = (category: string) =>
   axios.get<IList[]>(`api/${category}`);
